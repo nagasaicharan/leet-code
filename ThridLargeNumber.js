1f2bb64bafbbd9e4f58d3fd27f4d6b228f59dd00
G Naga Sai Charan <nagasaicharan7@gmail.com>
2020-11-28 20:47:32 +0530

    Array || Third Largest number in an array

---
+++ ThridLargeNumber.js
@@ -0,0 +1,40 @@
+/** 
+Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+Input: [3, 2, 1]
+
+Output: 1
+
+Explanation: The third maximum is 1.
+Example 2:
+Input: [1, 2]
+
+Output: 2
+
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+Input: [2, 2, 3, 1]
+
+Output: 1
+
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+  nums.sort(function (a, b) {
+    return a - b;
+  });
+  let uniqueChars = nums.filter((c, index) => {
+    return nums.indexOf(c) === index;
+  });
+  return uniqueChars[uniqueChars.length - 3] != undefined
+    ? uniqueChars[uniqueChars.length - 3]
+    : uniqueChars[uniqueChars.length - 1];
+};
+thirdMax([3, 2, 1]);
